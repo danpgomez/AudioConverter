73d19e33f36ec41df063db58e36148f91a3631b9
danpgomez <6013282+danpgomez@users.noreply.github.com>
2023-11-20 19:00:07 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("ws.schild:jave-all-deps:3.4.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/AudioConverter.kt
@@ -0,0 +1,76 @@
+import kotlinx.coroutines.*
+import ws.schild.jave.Encoder
+import ws.schild.jave.MultimediaObject
+import ws.schild.jave.encode.AudioAttributes
+import ws.schild.jave.encode.EncodingAttributes
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+class AudioConverter {
+    suspend fun convertWavToMp3(inputDirectory: File, outputDirectory: File) = coroutineScope {
+        launch {
+            // Code in here won't block the main thread.
+            outputDirectory.mkdirs()
+
+            if (!inputDirectory.exists() || !inputDirectory.isDirectory) {
+                println("Input directory does not exist or is not a directory.")
+                return@launch
+            }
+
+            val files = inputDirectory.listFiles { file ->
+                file.extension == "wav"
+            } ?: arrayOf()
+
+            val jobs = files.map { inputFile ->
+                async(Dispatchers.IO) {
+                    processFile(inputFile, outputDirectory)
+                }
+            }
+
+            jobs.awaitAll()
+        }
+    }
+
+    private fun createAudioAttributes(): AudioAttributes {
+        val audioAttributes = AudioAttributes()
+        audioAttributes.setCodec("libmp3lame")
+        audioAttributes.setBitRate(128000) // 128 kbps
+        audioAttributes.setChannels(2)
+        audioAttributes.setSamplingRate(44100)
+        return audioAttributes
+    }
+
+    private fun createEncodingAttributes(audioAttributes: AudioAttributes): EncodingAttributes {
+        val encodingAttributes = EncodingAttributes()
+        encodingAttributes.setOutputFormat("mp3")
+        encodingAttributes.setAudioAttributes(audioAttributes)
+        return encodingAttributes
+    }
+
+    private fun processFile(inputFile: File, outputDirectory: File) {
+        try {
+            val outputFileName = "${inputFile.nameWithoutExtension}.mp3"
+            val outputFile = File(outputDirectory, outputFileName)
+
+            val audioAttributes = createAudioAttributes()
+            val encodingAttributes = createEncodingAttributes(audioAttributes)
+
+            Encoder().encode(MultimediaObject(inputFile), outputFile, encodingAttributes)
+
+        } catch (exception: Exception) {
+            println("Error converting file ${inputFile.name}: ${exception.message}")
+        }
+    }
+}
+
+fun main() {
+    runBlocking {
+        val inputDirectoryPath = File("/Users/danpgomez/Desktop/WAV_Files")
+        val outputDirectoryPath = File("/Users/danpgomez/Desktop/WAV_Files/MP3s")
+
+        val time = measureTimeMillis {
+            AudioConverter().convertWavToMp3(inputDirectoryPath, outputDirectoryPath)
+        }
+        println("Completed in $time ms")
+    }
+}
